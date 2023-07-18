cb2a2bd08c9c0cdbc2a194064150753f37d9100c
Leon Nissen <>
2023-07-18 13:24:08 +0200

    add `toCsv` in FTData

--- Sources/FTKit/Models/FTData.swift
+++ Sources/FTKit/Models/FTData.swift
@@ -9,4 +9,29 @@ public struct FTData: Codable {
     public let distanceToScreen: Double?
     public let lookAtPoint: [String: Double]?
     public let faceGeometryVertices: [simd_float3]?
+    
+    public static let csvHeader = "type,timestamp,blendshapes,lightestimate,distanceToScreen,lookAtPoint,faceGeometryVertices"
+    
+    public func toCsv() -> String {
+        let blendshapeString = blendShapes?.toString() ?? "[]"
+        let lightEstimateString = lightEstimate?.toString() ?? "[]"
+        let distanceToScreenString = distanceToScreen.map { String($0) } ?? "nil"
+        let lookAtPointString = lookAtPoint?.toString() ?? "[]"
+        let faceGeometryVerticesString = faceGeometryVertices?.map { $0.toString() }.joined(separator: "_") ?? "[]"
+        
+        return "facetracking,\(timestamp),\(blendshapeString),\(lightEstimateString),\(distanceToScreenString),\(lookAtPointString),\(faceGeometryVerticesString)"
+    }
+}
+
+
+fileprivate extension Dictionary where Key == String, Value == Double {
+    func toString() -> String {
+        self.map { "\($0.key):\($0.value)" }.joined(separator: "_")
+    }
+}
+
+fileprivate extension simd_float3 {
+    func toString() -> String {
+        return "(\(self.x)-\(self.y)-\(self.z))"
+    }
 }
